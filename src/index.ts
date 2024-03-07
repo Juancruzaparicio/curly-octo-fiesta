66734603d0521a9102d0e211b169c15fb2a45d62
Juancruzaparicio <101576137+Juancruzaparicio@users.noreply.github.com>
2024-03-07 12:21:02 -0300

    cambios en imagen y resolucion

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,72 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Sprite } from 'pixi.js';
+import { Assets } from 'pixi.js';
 
+// Crear una instancia de la aplicación
 const app = new Application<HTMLCanvasElement>({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+    view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+    resolution: window.devicePixelRatio || 1,
+    autoDensity: true,
+    backgroundColor: 0x6495ed,
+    width: 1280,
+    height: 720
 });
 
-const clampy: Sprite = Sprite.from("./clampy.png");
+window.addEventListener("resize", ()=>{
+	const scalex = window.innerWidth / app.screen.width;
+	const scaley =  window.innerHeight / app.screen.height;
+	const scale = Math.min(scalex, scaley)
 
-clampy.anchor.set(0.5);
+	const gamewidth = Math.round(app.screen.width * scale);
+	const gameheight = Math.round(app.screen.height * scale);
 
-clampy.x = 300;
-clampy.y = 300;
+	const marginhorizontal = Math.floor((window.innerWidth - gamewidth) / 2);
+	const marginvertical = Math.floor((window.innerHeight - gameheight) / 2);
 
-app.stage.addChild(clampy);
+	app.view.style.width= gamewidth + "px";
+	app.view.style.height= gameheight + "px";
+
+	app.view.style.marginLeft = marginhorizontal + "px";
+	app.view.style.marginRight = marginhorizontal + "px";
+	app.view.style.marginTop = marginvertical + "px";
+	app.view.style.marginBottom = marginvertical + "px";
+})
+window.dispatchEvent( new Event("resize"));
+
+// Crear un manifest con los activos que deseas cargar
+const manifest = {
+    bundles: [
+        {
+            name: "bundleName",
+            assets: {
+                "myMessi": "./messi.jpg",
+                "myClampy": "./clampy.png",
+            }
+        },
+    ]
+};
+
+// Función para inicializar y cargar los activos
+async function init() {
+    // Inicializar Assets con el manifest
+    await Assets.init({ manifest: manifest });
+
+    // Cargar el bundle que contiene los activos
+    await Assets.loadBundle("bundleName");
+
+    // Crear los sprites una vez que los activos estén cargados
+    const messi: Sprite = Sprite.from("myMessi");
+    messi.x = 400;
+    messi.y = 40;
+
+	messi.scale.x = 1;
+	messi.scale.y = 1;
+	
+	messi.angle= 45;
+
+	const clampy: Sprite = Sprite.from("myClampy");
+	app.stage.addChild(clampy);
+    app.stage.addChild(messi);
+}
+
+// Llamar a la función init para comenzar la carga de activos
+init();
